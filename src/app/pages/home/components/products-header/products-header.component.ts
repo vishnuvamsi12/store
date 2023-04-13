56a1d1a078c8cfe6925dc73153b46d3d19aa6975
vishnuvamsi12 <vishnuvamsi8074@gmail.com>
2023-04-13 21:30:32 +0530

    WebShop Complete app

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: 'products-header.component.html', 
+  styles: [
+  ]
+})
+export class ProductsHeaderComponent implements OnInit{
+  @Output() columnsCountChange = new EventEmitter<number>();
+  @Output() itemsCountChange = new EventEmitter<number>();
+  @Output() sortChange = new EventEmitter<string>();
+
+  sort = 'sort';
+  itemsShowCount = 12;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+    this.sortChange.emit(newSort);
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+    this.itemsCountChange.emit(count);
+  }
+
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum);
+  }
+
+}
